5bcea9d1b7f0786981a6d4411edeca0a199c0a98
mihailgaberov <mihail.gaberov@gmail.com>
2024-11-20 17:20:59 +0100

    :sparkles: load 21 images => 7 x 3, update favicon

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
     const fetchData = async () => {
       try {
         const response = await fetch(
-          "https://api.nekosia.cat/api/v1/images/catgril?count=20&additionalTags=white-hair,uniform&blacklistedTags=short-hair,sad,maid"
+          "https://api.nekosia.cat/api/v1/images/catgril?count=21&additionalTags=white-hair,uniform&blacklistedTags=short-hair,sad,maid"
         );
         if (!response.ok) throw new Error("Failed to fetch data");
         const result = await response.json();
