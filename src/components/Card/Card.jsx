88e4a638553a475a850a52b29f11b290ec2f4076
mihailgaberov <mihail.gaberov@gmail.com>
2024-11-21 11:27:17 +0100

    :recycle: remove category names from the cards

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,12 +3,9 @@ import React from "react";
 import styles from "./Card.module.scss";
 
 function Card({ imgUrl, categoryName, randomizeOrder }) {
-
-
   return (
     <div className={styles.container} onClick={randomizeOrder}>
       <img src={imgUrl} alt={categoryName} id="img" />
-      <label htmlFor="img">{categoryName}</label>
     </div>
   );
 }
