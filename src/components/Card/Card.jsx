3ee1d5408936ae16eb24212e155bc4b5aa3716ef
mihailgaberov <mihail.gaberov@gmail.com>
2024-11-23 10:31:22 +0100

    :sparkles: add loader for each card

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import Loader from "../Loader";
 
 import styles from "./Card.module.scss";
 
 function Card({ imgUrl, imageId, categoryName, processTurn }) {
+  const [isLoading, setIsLoading] = useState(true);
+
+  const handleImageLoad = () => {
+    setIsLoading(false);
+  };
+
   return (
     <div className={styles.container} onClick={() => processTurn(imageId)}>
-      <img src={imgUrl} alt={categoryName} id="img" />
+      {isLoading && <Loader message="Loading image..." />}
+      <img
+        src={imgUrl}
+        alt={categoryName}
+        onLoad={handleImageLoad}
+        style={{ display: isLoading ? 'none' : 'block' }}
+      />
     </div>
   );
 }
