6eb3e455accb9b6e10196a35e456b6a3d74d7b28
mihailgaberov <mihail.gaberov@gmail.com>
2024-11-23 11:38:25 +0100

    :sparkles: Add version to the footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import styles from './Footer.module.scss';
+import packageJson from '../../../package.json';
 
 function Footer() {
   return (
     <footer className={styles.footer}>
       <p>
-        © 2024{' '}
+        2024{' '}
         <a
           href="https://github.com/mihailgaberov"
           target="_blank"
@@ -13,6 +14,7 @@ function Footer() {
         >
           Mihail Gaberov
         </a>
+        <span className={styles.version}>v{packageJson.version}</span>
       </p>
     </footer>
   );
